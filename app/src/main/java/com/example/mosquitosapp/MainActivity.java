fc7929552ecd7b3ad94dd7c55ed61245d183a2d0
Andrea <andrea_leonice@hotmail.com>
2022-10-25 12:51:31 -0300

    Adding tone generator

--- app/src/main/java/com/example/mosquitosapp/MainActivity.java
+++ app/src/main/java/com/example/mosquitosapp/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.mosquitosapp;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    EditText txt;
     MediaPlayer music;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,18 +20,42 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         music = MediaPlayer.create(this,
                 R.raw.sound);
+        txt = (EditText)findViewById(R.id.editTextNumber);
 
     }
+
     Boolean b = false;
 
       public void playMusic(View v) {
           if (!b) {
               music.start();
+
               b=true;
           } else {
               music.pause();
               b=false;
           }
       }
+    public void freqSound(View v){
+    final int duration = 10; // duration of sound in seconds
+    final int sampleRate = 8000; // Hz (maximum frequency is 7902.13Hz (B8))
+    final int numSamples = duration * sampleRate;
+    final double samples[] = new double[numSamples];
+    final short buffer[] = new short[numSamples];
+    final double freq = Double.parseDouble(txt.getText().toString()); //Hz
+    for (int i = 0; i < numSamples; ++i) {
+        samples[i] = Math.sin(2 * Math.PI * i / (sampleRate / freq)); // Sine wave
+        buffer[i] = (short) (samples[i] * Short.MAX_VALUE);  // Higher amplitude increases volume
+    }
 
-        }
+        AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, buffer.length, AudioTrack.MODE_STATIC);
+        audioTrack.write(buffer, 0, buffer.length);
+        audioTrack.play();
+
+
+    }
+
+
+}
