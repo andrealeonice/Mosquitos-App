128a1ac968e28f3233de939a1f1eef692d674d6e
andrealeonice <andrea_leonice@hotmail.com>
2023-03-01 16:35:25 -0300

    fixing wave formula

--- app/src/main/java/com/example/mosquitosapp/MainActivity.java
+++ app/src/main/java/com/example/mosquitosapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.mosquitosapp;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
-import android.media.MediaPlayer;
+import android.net.rtp.AudioStream;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -12,50 +12,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    EditText txt;
-    MediaPlayer music;
+    EditText txt,txt2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        music = MediaPlayer.create(this,
-                R.raw.sound);
         txt = (EditText)findViewById(R.id.editTextNumber);
-
+        txt2 = (EditText)findViewById(R.id.editTextNumber2);
     }
 
-    Boolean b = false;
 
-      public void playMusic(View v) {
-          if (!b) {
-              music.start();
 
-              b=true;
-          } else {
-              music.pause();
-              b=false;
-          }
-      }
+
     public void freqSound(View v){
-    final int duration = 200; // duration of sound in seconds
-    final int sampleRate = 8000; // Hz (maximum frequency is 7902.13Hz (B8))
-    final int numSamples = duration * sampleRate;
-    final double samples[] = new double[numSamples];
-    final short buffer[] = new short[numSamples];
-    final double freq = Double.parseDouble(txt.getText().toString()); //Hz
-    for (int i = 0; i < numSamples; ++i) {
-        samples[i] = Math.sin(2 * Math.PI * i / (sampleRate / freq)); // Sine wave
-        buffer[i] = (short) (samples[i] * Short.MAX_VALUE);  // Higher amplitude increases volume
-    }
+        final int duration = Integer.parseInt(txt2.getText().toString()); // duration of sound in seconds
+        int sampleRate = 44100;//44100; 12001// Hz (maximum frequency is 7902.13Hz (B8))
 
+        int bufferSize= AudioTrack.getMinBufferSize(sampleRate,
+                AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT);
         AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, buffer.length, AudioTrack.MODE_STATIC);
-        audioTrack.write(buffer, 0, buffer.length);
-        audioTrack.play();
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+
+
+    double samples =0;
+    final short buffer[] = new short[bufferSize*(duration/1000)];
+    final double freq = Double.parseDouble(txt.getText().toString()); //Hz
+    for (int i = 0; i < buffer.length; ++i) {
+        samples += 2 * Math.PI * freq /sampleRate; // Sine wave
+        buffer[i] = (short) (Math.sin(samples) * Short.MAX_VALUE);  // Higher amplitude increases volume
+    }
+
+           audioTrack.play(); // Inicia a reprodução
+            long startTime = (System.currentTimeMillis()*1000); // Tempo de início da reprodução
+
+
+            while (System.currentTimeMillis()*1000 - startTime < duration/1000) {
+                audioTrack.write(buffer, 0, buffer.length); // Escreve o buffer de amostras no AudioTrack
+            }
 
 
     }
-
-
 }
