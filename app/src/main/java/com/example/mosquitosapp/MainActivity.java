5a4fe86d2cccd3f2c61d8cdac27c13abb69fbf79
andrealeonice <andrea_leonice@hotmail.com>
2023-03-03 00:59:54 -0300

    inserting pause button

--- app/src/main/java/com/example/mosquitosapp/MainActivity.java
+++ app/src/main/java/com/example/mosquitosapp/MainActivity.java
@@ -14,6 +14,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     EditText txt, txt2;
+    AudioTrack audioTrack;
+    int sampleRate = 44100;//44100; 12001// Hz (maximum frequency is 7902.13Hz (B8))
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,41 +24,49 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         txt = (EditText) findViewById(R.id.editTextNumber);
         txt2 = (EditText) findViewById(R.id.editTextNumber2);
-        final Button bPlay = findViewById(R.id.button3);
+        //final Button bPlay = findViewById(R.id.button3);
         final Button bPause = findViewById(R.id.button);
 
-    }
-        private boolean b = false;
-        public void frequency(View v) {
-            int duration = Integer.parseInt(txt2.getText().toString()); // duration of sound in seconds
-            int sampleRate = 44100;//44100; 12001// Hz (maximum frequency is 7902.13Hz (B8))
-            int bufferSize = (sampleRate * 1 * 16 * duration) / 8;
-
-                AudioTrack audioTrack  = new AudioTrack(AudioManager.STREAM_MUSIC,
-                        sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                        AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
-            if (!b) {
-                double samples = 0;
-                b=true;
-                final short buffer[] = new short[bufferSize / 2];
-                final double freq = Double.parseDouble(txt.getText().toString()); //Hz
-                for (int i = 0; i < buffer.length; ++i) {
-                    samples += 2 * Math.PI * freq / sampleRate; // Sine wave
-                    buffer[i] = (short) (Math.sin(samples) * Short.MAX_VALUE);  // Higher amplitude increases volume
+        bPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (b) {
+                    audioTrack.pause();
+                    b = false;
                 }
-
-                audioTrack.play(); // Inicia a reprodução
-                audioTrack.write(buffer, 0, buffer.length); // Escreve o buffer de amostras no AudioTrack
-            } else {
-                b=false;
-                audioTrack.stop();
-                audioTrack.release();
             }
+        });
+
+    }
+
+    boolean b = false;
+
+    public void frequency(View v) {
+
+        int duration = Integer.parseInt(txt2.getText().toString()); // duration of sound in seconds
+        int bufferSize = (sampleRate * 1 * 16 * duration) / 8;
+
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, bufferSize, AudioTrack.MODE_STREAM);
+        final short buffer[] = new short[bufferSize / 2];
+        final double freq = Double.parseDouble(txt.getText().toString());
+
+        if (!b) {
+            double samples = 0;
+            for (int i = 0; i < buffer.length; ++i) {
+                samples += 2 * Math.PI * freq / sampleRate; // Sine wave
+                buffer[i] = (short) (Math.sin(samples) * Short.MAX_VALUE);  // Higher amplitude increases volume
+            }
+
+            // Inicia a reprodução
+            audioTrack.write(buffer, 0, buffer.length); // Escreve o buffer de amostras no AudioTrack
+            audioTrack.play();
+            b = true;
+
         }
 
 
-
-
-
     }
+}
 
